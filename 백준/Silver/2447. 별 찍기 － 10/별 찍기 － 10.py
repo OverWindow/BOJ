f5160ba96c1cec498c1bbeffa87590a58c1c3a39
OverWindow <101511418+OverWindow@users.noreply.github.com>
2022-03-30 01:34:59 +0900

    [Silver I] Title: 별 찍기 - 10, Time: 2440 ms, Memory: 69088 KB -BaekjoonHub

--- 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,31 +1,40 @@
-def draw_star(n) :
+N=int(input())
+Map = [[0 for i in range(N)]for i in range(N)]
+a = 1
+
+def star(n):
     global Map
-    
-    if n == 3 :
-        Map[0][:3] = Map[2][:3] = [1]*3
-        Map[1][:3] = [1, 0, 1]
+    global a
+    a *=3
+    if a==n:
         return
+    
+    for i in range(3):
+        for j in range(3):
+            if i == 1 and j == 1:
+                pass
+            else:
+                for k in range(a):
+                    Map[a*i+k][a*j:a*(j+1)] = Map[k][:a]
+    star(n)
 
-    a = n//3
-    draw_star(n//3)
-    for i in range(3) :
-        for j in range(3) :
-            if i == 1 and j == 1 :
-                continue
-            for k in range(a) :
-                Map[a*i+k][a*j:a*(j+1)] = Map[k][:a] # 핵심 아이디어
-
-N = int(input())      
-
-# 메인 데이터 선언
-Map = [[0 for i in range(N)] for i in range(N)]
-
-draw_star(N)
-
-for i in Map :
-    for j in i :
-        if j :
-            print('*', end = '')
-        else :
-            print(' ', end = '')
+    
+if N == 3:
+    Map[0][:3]=[1]*3
+    Map[1][:3]=[1,0,1]
+    Map[2][:3]=[1]*3
+else:
+    Map[0][:3]=[1]*3
+    Map[1][:3]=[1,0,1]
+    Map[2][:3]=[1]*3
+    star(N)
+    
+for i in Map:
+    for j in i:
+        if j == 1:
+            print("*",end='')
+        else:
+            print(" ",end='')
     print()
+
+print()
