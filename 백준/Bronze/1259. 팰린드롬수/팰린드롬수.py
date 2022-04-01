8c1d4bd78b07c3b289a7053c30f5da14c802ad12
OverWindow <101511418+OverWindow@users.noreply.github.com>
2022-04-01 14:40:10 +0900

    [Bronze I] Title: 팰린드롬수, Time: 72 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -4,22 +4,9 @@ while True:
         break
     else:
         L = [*str(N)]
-        if len(str(N)) % 2 == 0:
-            pass
-        else:
-            del L[len(L)//2]
-            
-        for i in range(0,int(len(L)/2)):
-            if L[i] == L[-1-i]:
-                L[i] = 1
-                L[-1-i] = 1
-            else:
-                L[i] = 0
-                L[-1-i] = 0
-        if sum(L) == len(L) or len(L) == 1:
+        if L == list(reversed(L)):
             print("yes")
         else:
             print("no")
                     
     
-
