6c1220946248734512c1f13ad0d76ed3628ef1c5
OverWindow <101511418+OverWindow@users.noreply.github.com>
2022-04-04 01:35:35 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,17 +1,9 @@
-Alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-word = list(input())
+Alphabet='abcdefghijklmnopqrstuvwxyz'
+word = input()
 
 for i in Alphabet:
     if i in word:
-        if word.count(i) == 2:
-            count = 0
-            while True:
-                if word[count] == i:
-                    break
-                count += 1
-            print(count,end=' ')     
-        else:
-            print(word.index(i),end=' ')
+        print(word.index(i),end=' ')
     else:
         print("-1",end=' ')
 
