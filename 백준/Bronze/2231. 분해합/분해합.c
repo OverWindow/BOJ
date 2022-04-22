b44c24d7e07c096d1ba5607fb8cccf9b585fd691
OverWindow <101511418+OverWindow@users.noreply.github.com>
2022-04-23 02:55:16 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.c
+++ 백준/Bronze/2231. 분해합/분해합.c
@@ -1,38 +1,28 @@
 #include <stdio.h>
 
 int main(void){
-    int n,min = -1;
-    
-    scanf("%d",&n);
-    
-    for (int every = 1; every <= n; every+=1)
-    {
-	    int j=1,copy_every,sum = every,arr[8] = {0};
-	    copy_every = every;
-	    
-	    // 각 자리수로 분류하기 
-	    for (int i = 1000000; i >=1; i = i / 10)
-	    {
-	    	arr[j] = copy_every / i;
-	    	copy_every -= (copy_every / i) * i;
-	    	j += 1;
-		}
-		
-		//각 자리수 다 더하기 
-		for (int k = 1; k <= 7; k++)
-			sum += arr[k];
-		
-		if (sum == n)
-		{
-			min = every;
-			break;
-		}
-	}
-	
-	if (min < 0)
-		min = 0;
-		
-	printf("%d",min);
+    int n=0;
+    scanf("%d", &n);
+    int flagg=0;
 
+    for(int i=1; i<n; i++)
+    {
+        int sum=0;
+        sum+=i;
+        int m=i;
+        while(m!=0)
+        {
+            sum+=m%10;
+            m/=10;
+        }
+        if(sum==n)
+        {
+            printf("%d\n", i);
+            flagg+=1;
+            break;
+        }
+    }
+    if(flagg==0)
+        printf("0");
 	return 0;
 }
