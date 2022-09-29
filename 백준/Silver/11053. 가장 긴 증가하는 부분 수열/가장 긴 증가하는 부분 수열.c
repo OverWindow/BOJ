386aa58fad75da1b8a20e7b142218a39ec683dcf
OverWindow <101511418+OverWindow@users.noreply.github.com>
2022-09-29 21:27:18 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.c
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.c
@@ -1,63 +1,37 @@
-#include <stdio.h>
+#include <stdio.h> 
 
-int max(int a, int b);
-
-int main(void){
-
-    int N;
-
-    scanf("%d", &N);
-
-    int A[N];
-
-    for(int i=0; i<N; i++){
-
-        scanf("%d", &A[i]);
-
-    }
-
-    int LIS[N]; //LIS[i]: A[i]를 포함하는 가장 긴 부분 수열의 길이
-
-    LIS[0]=1;  
-
-    for(int i=1; i<N; i++){
-
-        LIS[i]=1;
-
-        for(int j=0; j<i; j++){
-
-            if(A[i]>A[j]){
-
-                LIS[i] = max(LIS[i], LIS[j]+1);
-
-            }
-
-        }
-
-    }
-
-    int max=0;
-
-    for(int i=0; i<N; i++){
-
-        if(LIS[i]>max)
-
-            max=LIS[i];
-
-    }
-
-    printf("%d\n", max);
-
+int bigger(int a, int b) {
+	return a>b ? a : b;
 }
 
-int max(int a, int b){
-
-    if(a>b)
-
-        return a;
-
-    else
-
-        return b;
-
+int main(void) {
+	int N;
+	scanf("%d",&N);
+	
+	int arr[N+1];
+	arr[0] = 0;
+	for(int i = 1; i <= N; i++) {
+		scanf("%d",&arr[i]);
+	}
+	
+	int dp[N+1];
+	dp[0] = 0;
+	for(int i = 1; i <= N; i++) {
+		dp[i] = 1;
+		for(int j = 0; j < i; j++) {
+			if(arr[i] > arr[j]) {
+				dp[i] = bigger(dp[i],dp[j]+1);
+			}
+		}
+		//printf(":%d\n",dp[i]);
+	}
+	
+	int biggest = 1;
+	for(int i = 0 ; i <= N; i++) {
+		if(dp[i] > biggest) {
+			biggest = dp[i];
+		}
+	}
+	printf("%d\n",biggest);
+	return 0;
 }
