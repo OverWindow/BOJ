3bdff3345dac8375245fd52fef66105545ad311c
OverWindow <101511418+OverWindow@users.noreply.github.com>
2023-04-01 19:34:37 +0900

    [Silver V] Title: 배열 합치기, Time: 784 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -7,20 +7,45 @@ int main(void) {
 	int N,M;
 	scanf("%d %d",&N,&M);
 	
-	vector<int> A(N+M,0);
+	vector<int> A(N,0);
+	vector<int> B(M,0);
 	
 	for(int i = 0 ; i < N; i++) {
 		scanf("%d",&A[i]);
 	}
 	
-	for(int i = N; i < N+M; i++) {
-		scanf("%d",&A[i]);
+	for(int i = 0; i < M; i++) {
+		scanf("%d",&B[i]);
 	}
 	
-	sort(A.begin(),A.end());
+	vector<int> result(N+M,0);
+	
+	int i = 0; 
+	int j = 0;
+	int k = 0; 
+	while(1) {
+		if(i == N && j == M) {
+			break;
+		} else if(i == N) {
+			result[k] = B[j++];
+			k++;
+			continue;
+		} else if(j == M) {
+			result[k] = A[i++];
+			k++;
+			continue;
+		}
+		
+		if(A[i] > B[j]) {
+			result[k] = B[j++];
+		} else if (A[i] <= B[j]) {
+			result[k] = A[i++];
+		}
+		k++;
+	}
 	
 	for(int i = 0; i < N+M; i++) {
-		printf("%d ",A[i]);
+		printf("%d ",result[i]);
 	}
 	printf("\n");
 	return 0;
