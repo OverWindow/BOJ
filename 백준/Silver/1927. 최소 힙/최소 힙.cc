bb8f78996e0ad68bb3d541cf82c41db150a73b8b
OverWindow <101511418+OverWindow@users.noreply.github.com>
2023-09-07 23:29:45 +0900

    [Silver II] Title: 최소 힙, Time: 12 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#define MAX 100001
+using namespace std;
+
+int N;
+typedef struct {
+	int heap_size;
+	int heap[MAX];
+}HeapType;
+
+void push(HeapType *h, int item) {
+	int cur; 
+	h->heap_size = h->heap_size + 1;
+	cur = h->heap_size;
+	while(cur != 1 && item < h->heap[cur/2]) {
+		h->heap[cur] = h->heap[cur/2];
+		cur /= 2;
+	}
+	h->heap[cur] = item;
+	return;
+}
+
+int pop(HeapType *h) {
+	int parent,child;
+	int item,biggest;
+	biggest = h->heap[h->heap_size];
+	item = h->heap[1];
+	h->heap_size = h->heap_size - 1;
+	parent = 1;
+	child = 2;
+	while(child <= h->heap_size) {
+		if(child < h->heap_size && h->heap[child] > h->heap[child + 1]) {
+			child += 1;
+		}
+		if(biggest <= h->heap[child]) break;
+		h->heap[parent] = h->heap[child];
+		parent = child;
+		child *= 2;
+	}
+	h->heap[parent] = biggest;
+	return item;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> N;
+	HeapType h;
+	h.heap_size = 0;
+	
+	for(int i = 0; i < N; i++) {
+		int tmp;
+		cin >> tmp;
+		if(tmp == 0 && h.heap_size == 0) {
+			cout << 0 << "\n";
+		} else if(tmp == 0) {
+			cout << pop(&h) << "\n";
+		} else {
+			push(&h,tmp);
+		}
+	}
+	return 0;
+}
